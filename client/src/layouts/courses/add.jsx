e8f7e8a7f5239facd238fda69bb134bd61b4dfdb
saadennigro <saadennigro@gmail.com>
2022-08-01 12:25:41 +0100

    solve selector problem and some tsya9

--- client/src/layouts/courses/add.jsx
+++ client/src/layouts/courses/add.jsx
@@ -10,13 +10,11 @@ import MDBox from "components/MDBox";
 import MDInput from "components/MDInput";
 import MDButton from "components/MDButton";
 import MDTypography from "components/MDTypography";
-import Box from "@mui/material/Box";
 import OutlinedInput from "@mui/material/OutlinedInput";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
-import Chip from "@mui/material/Chip";
 
 //import UseState Hook
 import { useState } from "react";
@@ -33,17 +31,6 @@ import { useEffect } from "react";
 import { allPartnersRoute } from "utils/APIRoutes";
 import { addCourssRoute } from "utils/APIRoutes";
 
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: 30 * 4.5,
-    },
-  },
-};
-
-function getStyles(provider, name, theme) {
-  return
-}
 
 function AddCourses({ closeAddModel }) {
   const navigate = useNavigate();
@@ -61,21 +48,22 @@ function AddCourses({ closeAddModel }) {
     nom: "",
     provider: {
       id: "",
-      name: ""
+      name: "",
     },
     description: "",
   });
 
   const [selectedProvider, setSelectedProvider] = useState({
     nom: "",
-    id: ""
+    id: "",
   });
 
-  const [providers, setProviders] = useState([{
-    id: "",
-    nom: ""
-  }])
-
+  const [providers, setProviders] = useState([
+    {
+      id: "",
+      nom: "",
+    },
+  ]);
 
   useEffect(() => {
     const getAllPartners = async () => {
@@ -87,7 +75,6 @@ function AddCourses({ closeAddModel }) {
     getAllPartners();
   }, []);
 
-
   const handleSubmit = async (event) => {
     const { nom, provider, description } = course;
     event.preventDefault();
@@ -97,11 +84,11 @@ function AddCourses({ closeAddModel }) {
         provider: provider.id,
         description,
       });
-    if (data.status) {
-      navigate("/dashboard");
-    } else {
-      toast.error(data.msg, toastOptions);
-    }
+      if (data.status) {
+        navigate("/dashboard");
+      } else {
+        toast.error(data.msg, toastOptions);
+      }
     }
   };
 
@@ -134,6 +121,8 @@ function AddCourses({ closeAddModel }) {
 
     return true;
   };
+
+  const names = ["Huawei", "Cisco", "Rachid", "Morad"];
 
   return (
     <Card sx={{ mt: "50px" }}>
@@ -185,36 +174,22 @@ function AddCourses({ closeAddModel }) {
 
             <MDBox mb={2} ml={2} sx={{ width: "50%" }}>
               <FormControl sx={{ width: "100%" }}>
-                <InputLabel id="demo-multiple-chip-label">Provider</InputLabel>
+                <InputLabel id="demo-simple-select-label">Provider</InputLabel>
                 <Select
                   name="provider"
                   sx={{ height: 45 }}
-                  labelId="demo-multiple-chip-label"
-                  id="demo-multiple-chip"
-                  single="true"
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
                   value={selectedProvider.name}
+                  label="Age"
                   onChange={(e) => handleSelectedProvider(e)}
                   input={
                     <OutlinedInput id="select-multiple-chip" label="Provider" />
                   }
-                  renderValue={(selected) => (
-                    <Box sx={{ display: "flex", gap: 0.5, height: 32 }}>
-                      <Chip key={selected.id} label={selected.name} />
-                    </Box>
-                  )}
-                  MenuProps={MenuProps}
                 >
                   {providers.map((provider) => (
-                    <MenuItem
-                      key={provider.id}
-                      value={provider}
-                      style={getStyles(
-                        provider.name,
-                        selectedProvider.name,
-                        theme
-                      )}
-                    >
-                      {provider.name}
+                    <MenuItem key={provider.id} value={provider}>
+                      {provider.nom}
                     </MenuItem>
                   ))}
                 </Select>
