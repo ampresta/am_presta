da2a234665d827bac2d51af2df4f53d39b22e0b4
salahar9 <salahar9@gmail.com>
2022-08-09 14:25:46 +0100

    middlewares

--- server/controllers/login/GetType.js
+++ server/controllers/login/GetType.js
@@ -1,8 +1,8 @@
 const db = require("../../config/database");
 const { SuperAdmin, Collaborateur } = db.models;
 module.exports = async (user) => {
-  const collab = await Collaborateur.findOne({ UserId: user.id });
-  const superadmin = await SuperAdmin.findOne({ UserId: user.id });
+  const collab = await Collaborateur.findOne({ where: { UserId: user.id } });
+  const superadmin = await SuperAdmin.findOne({ where: { UserId: user.id } });
   if (collab) {
     if (collab.admin) {
       type = "Societe";
