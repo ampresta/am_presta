35731cce1c2e08843f30faa39d94c8fadd570d74
salahar9 <salahar9@gmail.com>
2022-08-09 12:27:17 +0100

    fixing cascade

--- server/controllers/login/GetType.js
+++ server/controllers/login/GetType.js
@@ -4,9 +4,14 @@ module.exports = async (user) => {
   const collab = await Collaborateur.findOne({ UserId: user.id });
   const superadmin = await SuperAdmin.findOne({ UserId: user.id });
   if (collab) {
-    return "collab";
+    if (collab.admin) {
+      type = "Societe";
+    } else {
+      type = "Collab";
+    }
+    return type;
   } else if (superadmin) {
-    return "superadmin";
+    return "Superadmin";
   } else {
     return "Error";
   }
