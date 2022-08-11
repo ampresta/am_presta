bc56fb8ccda746ca7d57ba95dc60c6230fec5354
abdoessordo <abdoessordo01@gmail.com>
2022-08-11 03:05:57 +0100

    merged

--- client/src/layouts/courses/index.js
+++ client/src/layouts/courses/index.js
@@ -57,7 +57,7 @@ function Courses() {
         );
         columns = [
           "nom",
-          "provider",
+          "providerID",
           "description",
           "collabs",
           "sessions",
