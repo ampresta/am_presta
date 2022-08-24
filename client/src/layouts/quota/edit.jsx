25ffb061236385c76ca7d247a488156b6ad4b928
saadennigro <saadennigro@gmail.com>
2022-08-25 00:25:28 +0100

    setup quota

--- client/src/layouts/quota/edit.jsx
+++ client/src/layouts/quota/edit.jsx
@@ -25,7 +25,7 @@ import {
 // Material Dashboard 2 React contexts
 import { useMaterialUIController, setUpdater } from "context";
 
-function AddQuota({ openAddModel }) {
+function AddQuota({ openAddModel, companyID }) {
   const [providers, setProviders] = useState([
     {
       id: "",
@@ -33,35 +33,41 @@ function AddQuota({ openAddModel }) {
       quota: "",
     },
   ]);
+
   const [quota, setQuota] = useState(0);
+  const [result, setResult] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const [controller, dispatch] = useMaterialUIController();
-
   const { updater } = controller;
 
   useEffect(() => {
     const getAllPartners = async () => {
       const { data } = await axios.post(AllQuotaRoute);
-      console.log("quota", data.quotas);
-      setQuota(data);
+      if (data.status) {
+        setQuota(data.quotas);
+        const filter = data.quotas.reduce((r, a) => {
+          r[a.SocieteId] = r[a.SocieteId] || [];
+          r[a.SocieteId].push(a);
+          return r;
+        }, Object.create(null));
+        setResult(filter[companyID]);
+        setLoading(true);
+      }
     };
     getAllPartners();
   }, []);
 
-  // const result = quota.reduce((r, a) => {
-  //   r[a.SocieteId] = r[a.SocieteId] || [];
-  //   r[a.SocieteId].push(a);
-  //   return r;
-  // }, Object.create(null));
+  console.log("ID aba", companyID);
 
-  // console.log(result);
+  // result.map((item, key) => console.log(result[key].quota));
 
   useEffect(() => {
     const getAllPartners = async () => {
       const { data } = await axios.get(allPartnersRoute);
       let temp = [];
       data.map((provider) =>
-        temp.push({ id: provider.id, nom: provider.nom, quota: quota })
+        temp.push({ id: provider.id, nom: provider.nom, quota })
       );
       setProviders(temp);
     };
@@ -74,7 +80,7 @@ function AddQuota({ openAddModel }) {
     providers.map((provider) =>
       quotas.push({
         provider: provider.id,
-        societe: localStorage.getItem("companyID"),
+        societe: companyID,
         quota: provider.quota,
       })
     );
@@ -98,91 +104,93 @@ function AddQuota({ openAddModel }) {
 
   return (
     <Card sx={{ mt: "50px" }}>
-      <MDBox
-        display="flex"
-        justifyContent="space-between"
-        alignItems="center"
-        variant="gradient"
-        bgColor="info"
-        borderRadius="lg"
-        coloredShadow="info"
-        p={3}
-        mx={2}
-        mt={-3}
-        mb={1}
-      >
-        <MDTypography variant="h6" color="white">
-          {quota.length !== 0 ? "Add Quota" : "Edit Quota"}
-        </MDTypography>
-
-        <MDButton
-          variant="gradient"
-          color="dark"
-          size="small"
-          iconOnly
-          onClick={() => openAddModel(false)}
-        >
-          <Icon fontSize="small">close</Icon>
-        </MDButton>
-      </MDBox>
-
-      <MDBox pt={4} pb={3} px={10}>
-        <MDBox
-          component="form"
-          role="form"
-          onSubmit={(event) => handleSubmit(event)}
-        >
-          {providers.map((provider, index) => {
-            return (
-              <MDBox display="flex" key={provider.id}>
-                <MDBox mb={2} mr={2} s sx={{ width: "50%" }}>
-                  <MDInput
-                    type="text"
-                    value={provider.nom}
-                    variant="outlined"
-                    fullWidth
-                    InputProps={{
-                      readOnly: true,
-                    }}
-                  />
-                </MDBox>
-
-                <MDBox mb={2} ml={2} sx={{ width: "50%" }}>
-                  <MDInput
-                    type="number"
-                    label="Quantity"
-                    variant="outlined"
-                    fullWidth
-                    name="quota"
-                    defaultValue={provider.quota.quota}
-                    onChange={(event) => handleChange(index, event)}
-                  />
-                </MDBox>
-              </MDBox>
-            );
-          })}
-
-          <MDBox mt={4} mb={2} display="flex" justifyContent="center">
-            <MDButton
-              type="submit"
-              variant="gradient"
-              color="info"
-              sx={{ width: "30%", mr: "5px" }}
-            >
-              Submit
-            </MDButton>
+      {loading && (
+        <>
+          <MDBox
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            variant="gradient"
+            bgColor="info"
+            borderRadius="lg"
+            coloredShadow="info"
+            p={3}
+            mx={2}
+            mt={-3}
+            mb={1}
+          >
+            <MDTypography variant="h6" color="white">
+              {quota.length !== 0 ? "Add Quota" : "Edit Quota"}
+            </MDTypography>
 
             <MDButton
-              type="reset"
               variant="gradient"
               color="dark"
-              sx={{ width: "30%", ml: "5px" }}
+              size="small"
+              iconOnly
+              onClick={() => openAddModel(false)}
             >
-              clear
+              <Icon fontSize="small">close</Icon>
             </MDButton>
           </MDBox>
-        </MDBox>
-      </MDBox>
+
+          <MDBox pt={4} pb={3} px={10}>
+            <MDBox
+              component="form"
+              role="form"
+              onSubmit={(event) => handleSubmit(event)}
+            >
+              {providers.map((provider, index) => (
+                <MDBox display="flex" key={provider.id}>
+                  <MDBox mb={2} mr={2} s sx={{ width: "50%" }}>
+                    <MDInput
+                      type="text"
+                      value={provider.nom}
+                      variant="outlined"
+                      fullWidth
+                      InputProps={{
+                        readOnly: true,
+                      }}
+                    />
+                  </MDBox>
+
+                  <MDBox mb={2} ml={2} sx={{ width: "50%" }}>
+                    <MDInput
+                      type="number"
+                      label="Quantity"
+                      variant="outlined"
+                      fullWidth
+                      name="quota"
+                      // defaultValue={result[index].quota}  <--- hadi ra Vri walakin render laho ra2y akhar
+                      onChange={(event) => handleChange(index, event)}
+                    />
+                  </MDBox>
+                </MDBox>
+              ))}
+
+              <MDBox mt={4} mb={2} display="flex" justifyContent="center">
+                <MDButton
+                  type="submit"
+                  variant="gradient"
+                  color="info"
+                  sx={{ width: "30%", mr: "5px" }}
+                >
+                  Submit
+                </MDButton>
+
+                <MDButton
+                  type="reset"
+                  variant="gradient"
+                  color="dark"
+                  sx={{ width: "30%", ml: "5px" }}
+                >
+                  clear
+                </MDButton>
+              </MDBox>
+            </MDBox>
+          </MDBox>
+        </>
+      )}
     </Card>
   );
 }
