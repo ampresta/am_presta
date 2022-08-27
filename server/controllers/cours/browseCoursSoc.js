ac8e833c5a92a5114cfdc20fe7ae1981cd0d4e84
salahar9 <salahar9@gmail.com>
2022-08-27 16:17:25 +0100

    fixing courses

--- server/controllers/cours/browseCoursSoc.js
+++ server/controllers/cours/browseCoursSoc.js
@@ -1,133 +1,48 @@
-const sequelize = require("sequelize");
-const db = require("../../config/database");
-const {
-  Cours,
-  Proof,
-  Session_Collab,
-  Session,
-  Provider,
-  Collaborateur,
-  Quota,
-} = db.models;
+const { Op } = require("sequelize");
+const sequelize = require("../../config/database");
+const { Provider, Quota, Cours, Collaborateur } = sequelize.models;
 module.exports = async (req, res) => {
-  filters = {};
-
-  filters.include = [
-    {
-      model: Session,
-      where: {
-        SocieteId: req.societe,
-      },
-      required: false,
-      attributes: [],
+  const { provider } = req.body;
+  if (provider) {
+    let t = [];
+    for (i of provider) {
+      t.push({
+        id: i,
+      });
+    }
+    filters = {
       include: {
-        model: Session_Collab,
-        // required:false,
-        attributes: [],
-        include: [
-          {
-            model: Proof,
-            attributes: [],
-            as: "certifs",
-            required: false,
-            where: {
-              status: "accepted",
-            },
-          },
-          {
-            model: Collaborateur,
-            // required: false,
-            attributes: [],
-            where: {
-              admin: false,
-              instructor: false,
-            },
-          },
-        ],
-      },
-      // ],
-    },
-    {
-      model: Provider,
-      required: true,
-      attributes: ["id", "nom"],
-      include: {
-        model: Quota,
+        model: Provider,
+        where: {
+          [Op.or]: t,
+        },
+        attributes: ["nom"],
         required: true,
-        where: { SocieteId: req.societe },
-        attributes: [],
+        include: {
+          model: Quota,
+          required: false,
+          where: {
+            SocieteId: req.societe,
+          },
+        },
       },
-    },
-  ];
-  filters.attributes = {
-    include: [
-      [
-        sequelize.fn(
-          "count",
-          sequelize.fn("distinct", sequelize.col("Sessions.id"))
-        ),
-        "sessions",
-      ],
-      [
-        sequelize.fn(
-          "count",
-          //   sequelize.fn(
-          //     "distinct",
-          sequelize.col("Sessions->Session_Collabs->Collaborateur.id")
-          // )
-        ),
-        "collabs",
-      ],
-      [
-        sequelize.fn(
-          "count",
-          sequelize.fn(
-            "distinct",
-            sequelize.col("Sessions->Session_Collabs->certifs.id")
-          )
-        ),
-        "collabs_fin",
-      ],
-    ],
-  };
-  filters.group = ["Cours.id", "Provider.id"];
-  if (req.method == "POST") {
-    const { search, provider } = req.body;
-
-    if (search) {
-      filters.attributes.include.push([
-        sequelize.fn("similarity", sequelize.col("Cours.nom"), search),
-        "score",
-      ]);
-      filters.where = [
-        sequelize.where(
-          sequelize.fn("similarity", sequelize.col("Cours.nom"), search),
-          { [sequelize.Op.gt]: 0.1 }
-        ),
-      ];
-    }
-
-    if (provider) {
-      if (filters.where) {
-        temp = filters.where;
-        filters.where = {
-          [sequelize.Op.and]: [filters.where, { ProviderId: provider }],
-        };
-      } else {
-        filters.where = { ProviderId: provider };
-      }
-    }
-    console.log(filters);
-    try {
-      const cours = await Cours.findAll(filters); // Implementing search
-      return res.send({ status: true, cours });
-    } catch (err) {
-      console.log(err);
-      return res.send({ status: false });
-    }
+    };
   } else {
-    console.log(filters);
-    const cours = await Cours.findAll(filters); // Implementing search
-    return res.send({ status: true, cours });
+    filters = {
+      include: {
+        model: Provider,
+        attributes: ["nom"],
+        required: true,
+        include: {
+          model: Quota,
+          required: false,
+          where: {
+            SocieteId: req.societe,
+          },
+        },
+      },
+    };
   }
+  const cours = await Cours.findAll(filters);
+  return res.send({ status: true, cours });
 };
