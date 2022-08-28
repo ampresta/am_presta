4c99780db7708d9c50e9bfeebd52da8e74fad9fc
abdoessordo <abdoessordo01@gmail.com>
2022-08-28 16:03:13 +0100

    added api endpoint for reading notifs Soc and Collab

--- server/routes/notifs.js
+++ server/routes/notifs.js
@@ -1,9 +1,15 @@
 const { Router } = require("express");
 const addNotif = require("../controllers/nontifs/addNotif");
 const browseNotifsSoc = require("../controllers/nontifs/browseNotifsSoc");
+const readNotifsCollab = require("../controllers/nontifs/readNotifsCollab");
+const readNotifsSoc = require("../controllers/nontifs/readNotifsSoc");
 const router = Router();
 
 router.get("/add", addNotif);
 router.post("/browseSoc", browseNotifsSoc);
+
+
+router.post("/readNotifCollab", readNotifsCollab);
+router.post("/readNotifSoc", readNotifsSoc);
 
 module.exports = router;
