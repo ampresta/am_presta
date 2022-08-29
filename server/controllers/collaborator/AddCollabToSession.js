ae556bcafb3759a5ed3ce37d99c1611e918a2957
abdoessordo <abdoessordo01@gmail.com>
2022-08-29 16:51:39 +0100

    fix small bug in notifs

--- server/controllers/collaborator/AddCollabToSession.js
+++ server/controllers/collaborator/AddCollabToSession.js
@@ -58,6 +58,7 @@ module.exports = async (req, res) => {
   sess.Cour.Provider.Quota[0].save();
 
   const { nom } = await Session.findByPk(session);
+
   if (request) {
     const cours = await Cours.findOne({
       attributes: ["nom"],
