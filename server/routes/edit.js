3e656c2c310af56eb58120086e36715d6cb4142e
salahar9 <salahar9@gmail.com>
2022-09-03 18:11:29 +0100

    edit dept

--- server/routes/edit.js
+++ server/routes/edit.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
 const editCourses = require("../controllers/cours/editCourses");
 const editSessions = require("../controllers/session/editSession");
+const editDept = require("../controllers/departement/editDept");
 const router = Router();
 router.post("/course",editCourses)
 router.post("/session",editSessions)
+router.post("/dept",editDept)
 //router.post("/course",editCourses)
 
 // router.use(signedin);
